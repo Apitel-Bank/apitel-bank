1e58cad4ec6a9673f82b54b733f583cd6b1418fa
arinaho-muleba <157449847+arinaho-muleba@users.noreply.github.com>
2024-06-25 18:46:14 +0200

    Basic router set up

--- bank-manager-portal/src/App.js
+++ bank-manager-portal/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/navbar/navbar";
+import Accounts from "./routes/accounts/accounts";
+import Dashboard from "./routes/dashboard/dashbord";
+import Investments from "./routes/investments/investments";
+import Payments from "./routes/payments/payments";
+import Settings from "./routes/settings/settings";
+import Statements from "./routes/statements/statements";
+import Transactions from "./routes/transactions/transactions";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/accounts" element={<Accounts />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/investments" element={<Investments/>} />
+          <Route path="/payments" element={<Payments/>} />
+          <Route path="/settings" element={<Settings/>} />
+          <Route path="/statements" element={<Statements/>} />
+          <Route path="/transactions" element={<Transactions/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ bank-manager-portal/src/routes/statements/statements.jsx
@@ -0,0 +1,3 @@
+export default function Statements (){
+    return <h1>{"Statements"}</h1>
+}
