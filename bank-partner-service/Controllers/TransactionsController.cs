267e49467d361407d25a0df3726b67d7b0e75868
Batandwa Mgutsi <Batandwa.Mgutsi@bbd.co.za>
2024-06-26 10:18:34 +0200

    Updates API for partner service

---
+++ bank-partner-service/Controllers/TransactionsController.cs
@@ -0,0 +1,47 @@
+﻿using BankPartnerService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankPartnerService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        /// <summary>
+        /// Queues a payment order into the given bank and recepients.
+        /// 
+        /// This endpoint will not accept transactions whose references is not unique.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <response code="202">Accepted</response>
+        [HttpPost("Payments")]
+        public IActionResult MakePayment(QueuedRequest<MakePayment> request)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Queues deposits into the accounts for the given persona ids and amounts.
+        /// 
+        /// This endpoint will not accept transactions whose references is not unique.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <response code="202">Accepted</response>
+        [HttpPost("Deposits")]
+        public IActionResult MakeDeposit(QueuedRequest<IEnumerable<MakeDeposit>> request)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Returns status of deposit/payment with given reference.
+        /// </summary>
+        /// <response code="200">New|Accepted|Verified|Rejected|Reversed values for status</response>
+        [HttpGet("{Reference}/Status")]
+        public ActionResult<GetTransactionStatusResponse> GetTransactionStatus()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ bank-partner-service/Models/MakePayment.cs
@@ -0,0 +1,12 @@
+﻿namespace BankPartnerService.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="SenderId">If not given, CardNumber must be given.</param>
+    /// <param name="CardNumber">If not given, SenderId must be given</param>
+    /// <param name="AmountInMibiBBDough"></param>
+    /// <param name="Recepient"></param>
+    /// <param name="Reference"></param>
+    public record MakePayment(long? SenderId, string? CardNumber, long AmountInMibiBBDough, PaymentRecepient Recepient, string Reference);
+}
