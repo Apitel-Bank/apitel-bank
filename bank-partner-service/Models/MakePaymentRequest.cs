eeba5a0ebd48ea31f21db62c72f58d6f4c749b0e
Batandwa Mgutsi <Batandwa.Mgutsi@bbd.co.za>
2024-06-26 17:18:30 +0200

    Async deposit payments

--- bank-partner-service/Models/MakePaymentRequest.cs
+++ bank-partner-service/Models/MakePaymentRequest.cs
@@ -1,12 +1,4 @@
 ﻿namespace BankPartnerService.Models
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="SenderId">If not given, CardNumber must be given.</param>
-    /// <param name="CardNumber">If not given, SenderId must be given</param>
-    /// <param name="AmountInMibiBBDough"></param>
-    /// <param name="Recepient"></param>
-    /// <param name="Reference"></param>
-    public record MakePaymentRequest(long? SenderId, string? CardNumber, long AmountInMibiBBDough, PaymentRecepient Recepient, string Reference);
+    public record MakePaymentRequest(long? SenderId, long AmountInMibiBBDough, PaymentRecepient Recepient, string Reference);
 }
