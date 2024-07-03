d4e3d66f5962c25afe86a78207fc0e9c75e843e5
Liam Talberg <liamtalberg@gmail.com>
2024-07-03 10:42:34 +0200

    Add oauth protection on backend

--- reporting-service/src/main/java/com/apitel_bank/reporting_service/ReportingServiceApplication.java
+++ reporting-service/src/main/java/com/apitel_bank/reporting_service/ReportingServiceApplication.java
@@ -2,8 +2,10 @@ package com.apitel_bank.reporting_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class ReportingServiceApplication {
 
 	public static void main(String[] args) {
