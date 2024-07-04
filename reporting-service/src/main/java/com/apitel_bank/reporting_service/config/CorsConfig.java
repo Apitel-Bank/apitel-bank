b013b6de2faadc2d225284dfa9bc0361d53f0934
arinaho-muleba <157449847+arinaho-muleba@users.noreply.github.com>
2024-07-04 03:54:49 +0200

    Merge branch 'main' of https://github.com/Apitel-Bank/apitel-bank into arinaho/feature/statements&accounts-balances

--- reporting-service/src/main/java/com/apitel_bank/reporting_service/config/CorsConfig.java
+++ reporting-service/src/main/java/com/apitel_bank/reporting_service/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") 
+                .allowedOrigins("http://localhost:8080/","http://apitel-portal.s3-website-eu-west-1.amazonaws.com/") 
                 .allowedMethods("GET")
                 .allowedHeaders("*"); 
     }
